ad865d66fca73d8311cc60ca5c9a952b90b6485c
Daniel <danhab20@gmail.com>
2026-08-11 15:49:43 +0300

    Solution of modules

---
+++ JS/modules/logger.js
@@ -0,0 +1,8 @@
+export default function log(msg) {
+    console.log("[LOG]", msg)
+}
+
+
+export function superLog(msg) {
+    console.log("[{[LOOOOG]}]:", msg)
+}
---
+++ JS/modules/main.js
@@ -0,0 +1,8 @@
+import { shout } from "./strings.js";
+import myLogger, {superLog} from "./logger.js"
+
+console.log(shout("azag!"))
+
+myLogger("hi")
+superLog("AZAGGG")
+
