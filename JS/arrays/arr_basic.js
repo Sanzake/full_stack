cdbcc0df15cb068b92b4fc339d3107908c19bfae
Daniel <danhab20@gmail.com>
2026-08-11 11:44:58 +0300

    solutions of arrays

---
+++ JS/arrays/arr_basic.js
@@ -0,0 +1,6 @@
+const colors = ["red", "blue", "white"]
+
+colors.push("black")
+
+console.log(colors.length)
+console.log(colors[colors.length - 1])
---
+++ JS/arrays/loop_on_arr.js
@@ -0,0 +1,5 @@
+const nums = [10, 20, 30, 40]
+
+for (let i = 0; i <= nums.length - 1; i++) {
+    console.log(`${i}: ${nums[i]}`)
+}
